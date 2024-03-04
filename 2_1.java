19f3e1936b83e24da6916cb4c03ffe931f9bc9b2
HongWonSeok <85995027+HongWonSeok@users.noreply.github.com>
2024-03-04 03:53:40 +0000

    update

--- 2_1.java
+++ 2_1.java
@@ -1,7 +1,7 @@
 class test {
   public static void main(String[] args) {
     java.util.Scanner input = new java.util.Scanner(System.in);
-  
+    // 1
     System.out.print("Enter a miles: ");
     double miles = input.nextDouble();
     double kilometers = miles * 1.6;
